295e47825c67271c9e1ee723bf822a13fe0b5cef
midwinter1111 <kuro96fate@yahoo.co.jp>
2022-12-29 20:41:07 +0900

    サイトのテンプレートを作成

---
+++ js/scroll_up.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", function() {
+    const target = document.querySelectorAll('.scr-target')
+    const targetArray = Array.prototype.slice.call(target);
+    const options = {
+        root: null,
+        rootMargin: '0px 0px',
+        threshold: 0.2
+    };
+    
+    const observer = new IntersectionObserver(callback, options)
+    targetArray.forEach(function (tgt) {
+        observer.observe(tgt)
+    });
+    
+    function callback(entries) {
+        entries.forEach(function(entry) {
+            const target = entry.target;
+            if (entry.isIntersecting && !target.classList.contains('is-active')) {
+                target.classList.add('is-active');
+            }
+        });
+    };
+});
+
+window.addEventListener("load", function() {
+    const target = document.querySelectorAll('.scr-delay-target')
+    const targetArray = Array.prototype.slice.call(target);
+    const options = {
+        root: null,
+        rootMargin: '0px 0px',
+        threshold: 0.2
+    };
+    
+    const observer = new IntersectionObserver(callback, options)
+    targetArray.forEach(function (tgt) {
+        observer.observe(tgt)
+    });
+    
+    function callback(entries) {
+        entries.forEach(function(entry, i) {
+            const target = entry.target;
+            if (entry.isIntersecting && !target.classList.contains('is-active')) {
+                const delay = i * 100
+                setTimeout(function(){
+                    target.classList.add('is-active');
+                },delay);
+            }
+        });
+    };
+});
